3863884105b09f188e46871bc8454b748a3272a4
jipmez <mezj972@gmail.com>
2026-04-22 12:46:33 +0100

    update

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -1,30 +1,50 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('backend.include.__head')
+
 <body>
-<!--Full Layout-->
-<div class="layout">
+    <!--Full Layout-->
+    <div class="layout">
 
-   <x-notify::notify />
-    <!--Header-->
-    @include('backend.include.__header')
-    <!--/Header-->
+        <x-notify::notify />
+        <!--Header-->
+        @include('backend.include.__header')
+        <!--/Header-->
 
-    <!--Side Nav-->
-    @include('backend.include.__side_nav')
-    <!--/Side Nav-->
+        <!--Side Nav-->
+        @include('backend.include.__side_nav')
+        <!--/Side Nav-->
 
-    <!--Page Content-->
-    <div class="page-container">
-        @yield('content')
+        <!--Page Content-->
+        <div class="page-container">
+            @yield('content')
+        </div>
+        <!--Page Content-->
     </div>
-    <!--Page Content-->
-</div>
-<!--/Full Layout-->
+    <!--/Full Layout-->
 
-@include('backend.include.__script')
-<script src="//code.jivosite.com/widget/zAfVk6Avfk" async></script>
+    @include('backend.include.__script')
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = '9642f2e044606cc0fdb502e013d720da8ad3b830';
+        window.smartsupp || (function(d) {
+            var s, c, o = smartsupp = function() {
+                o._.push(arguments)
+            };
+            o._ = [];
+            s = d.getElementsByTagName('script')[0];
+            c = d.createElement('script');
+            c.type = 'text/javascript';
+            c.charset = 'utf-8';
+            c.async = true;
+            c.src = 'https://www.smartsuppchat.com/loader.js?';
+            s.parentNode.insertBefore(c, s);
+        })(document);
+    </script>
+    <noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
 
 
 </body>
+
 </html>
